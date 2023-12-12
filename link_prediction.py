690a7096f8174af8ec4ff697713685fab14f6cda
Luke Braithwaite <lukebraithwaite7@gmail.com>
2023-12-12 16:34:01 +0000

    :sparkles: link prediction code can now be ran on a GPU as a test

--- link_prediction.py
+++ link_prediction.py
@@ -1,5 +1,6 @@
 import lightning as L
 from lightning.pytorch.callbacks import EarlyStopping, ModelCheckpoint
+from lightning.pytorch.loggers import WandbLogger
 
 from datasets.link_pred import LastFMDataModule
 from models.HAN import HANLinkPredictor
@@ -14,15 +15,15 @@ def main():
     model = HANLinkPredictor(datamodule.metadata, hidden_channels=256,
                              edge_target=datamodule.target)
 
-    # logger = WandbLogger(project="gnn-baselines", log_model="all")
-    # logger.experiment.config["model"] = "HAN"
-    # logger.experiment.config["dataset"] = "MovieLens"
-    # logger.experiment.tags = ['GNN', 'baseline', 'link_prediction']
+    logger = WandbLogger(project="gnn-baselines", log_model="all")
+    logger.experiment.config["model"] = "HAN"
+    logger.experiment.config["dataset"] = "LastFM"
+    logger.experiment.tags = ['GNN', 'baseline', 'link_prediction']
 
     trainer = L.Trainer(accelerator="cpu", log_every_n_steps=1,
                         max_epochs=200,
-                        # fast_dev_run=True,
-                        # logger=logger,
+                        fast_dev_run=True,
+                        logger=logger,
                         callbacks=[EarlyStopping("valid/loss", patience=100),
                                    ModelCheckpoint(monitor="valid/accuracy",
                                                    mode="max")])
