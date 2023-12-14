6d722f8d1d2aa8004c3c7db57b758fdfea6f6537
Luke Braithwaite <lukebraithwaite7@gmail.com>
2023-12-14 14:18:57 +0000

    :sparkles: fixed slurm script

--- slurm_submission_script.sh
+++ slurm_submission_script.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-#SBATCH -A COMPUTERLAB-GPU
+#SBATCH -A COMPUTERLAB-SL2-GPU
 #SBATCH --time=01:00:00
 #SBATCH -p ampere
 #SBATCH --nodes 1
 #SBATCH --gres=gpu:4
 #SBATCH --exclusive
 
-python link_prediction.py
+srun python link_prediction.py
