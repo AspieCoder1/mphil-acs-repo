df4151b201441629ce7d7f86667ccd00c9da0c9d
Luke Braithwaite <lukebraithwaite7@gmail.com>
2023-12-24 10:56:05 +0000

    :sparkles: added reference to twitter codebase for sheafs

--- models/SheafGNN/__init__.py
+++ models/SheafGNN/__init__.py
@@ -1,2 +1,3 @@
-# Copyright 2022 Twitter, Inc.
-# SPDX-License-Identifier: Apache-2.0
+# Copyright 2022 Twitter, Inc. SPDX-License-Identifier: Apache-2.0
+# https://github.com/twitter-research/neural-sheaf-diffusion
+# Bodnar et al. (NeurIPS 2022)
