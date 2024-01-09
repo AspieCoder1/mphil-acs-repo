181a1b79253e113ff324dbf8df4f3094189e425b
Luke Braithwaite <lukebraithwaite7@gmail.com>
2024-01-09 16:22:59 +0000

    :sparkles: Added log_every_n_steps control to link_prediction.py

--- link_prediction.py
+++ link_prediction.py
@@ -5,7 +5,6 @@ import lightning as L
 import torch
 from hydra.core.config_store import ConfigStore
 from lightning.pytorch.callbacks import EarlyStopping, ModelCheckpoint
-from lightning.pytorch.loggers import WandbLogger
 
 from core.datasets import get_dataset_lp, LinkPredDatasets
 from core.models import Models, get_model
@@ -36,19 +35,19 @@ def main(cfg: Config):
                                    homogeneous=is_homogeneous,
                                    batch_size=datamodule.batch_size)
 
-    # logger = WandbLogger(project="gnn-baselines", log_model=True)
-    # logger.experiment.config["model"] = cfg.model
-    # logger.experiment.config["dataset"] = cfg.dataset
-    # logger.experiment.tags = ['GNN', 'baseline', 'link_prediction']
+    logger = WandbLogger(project="gnn-baselines", log_model=True)
+    logger.experiment.config["model"] = cfg.model
+    logger.experiment.config["dataset"] = cfg.dataset
+    logger.experiment.tags = ['GNN', 'baseline', 'link_prediction']
 
-    trainer = L.Trainer(log_every_n_steps=1,
+    trainer = L.Trainer(log_every_n_steps=cfg.trainer.log_every_n_steps,
                         num_nodes=cfg.trainer.num_nodes,
                         accelerator=cfg.trainer.accelerator,
                         devices=cfg.trainer.devices,
                         strategy=cfg.trainer.strategy,
                         fast_dev_run=cfg.trainer.fast_dev_run,
                         max_epochs=200,
-                        # logger=logger,
+                        logger=logger,
                         callbacks=[
                             EarlyStopping("valid/loss", patience=cfg.trainer.patience),
                             ModelCheckpoint(monitor="valid/accuracy",
