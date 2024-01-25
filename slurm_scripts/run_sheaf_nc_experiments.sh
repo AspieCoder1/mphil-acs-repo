545563e8a6f89eae1eeb1e7408d8bb96cf28e6b2
Luke Braithwaite <lukebraithwaite7@gmail.com>
2024-01-25 12:13:23 +0000

    :sparkles: fixed model info for WANDB

--- slurm_scripts/run_sheaf_nc_experiments.sh
+++ slurm_scripts/run_sheaf_nc_experiments.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH -J sheafnc_baselines
-#SBATCH --output=/out/sheafnc_baselines/%A_%a.out
-#SBATCH --error=/err/sheafnc_baselines/%A_%a.err
+#SBATCH --output=sheafnc_baselines/%A_%a.out
+#SBATCH --error=sheafnc_baselines/%A_%a.err
 #SBATCH -A COMPUTERLAB-SL2-GPU
 #SBATCH --time=01:00:00
 #SBATCH -a 0-89%10
