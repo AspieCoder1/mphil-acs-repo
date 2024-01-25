6ea613d901992d522c8fecf8dca38aa1bcd03c04
Luke Braithwaite <lukebraithwaite7@gmail.com>
2024-01-25 10:53:52 +0000

    :wrench: added slurm submission script for baseline runs

---
+++ slurm_scripts/run_sheaf_nc_experiments.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#SBATCH -J sheafnc_baselines
+#SBATCH --output=out/sheafnc_baselines_%A_%a.out
+#SBATCH --error=err/sheafnc_baselines_%A_%a.err
+#SBATCH -A COMPUTERLAB-SL2-GPU
+#SBATCH --time=01:00:00
+#SBATCH -a 0-89%10
+#SBATCH -p ampere
+#SBATCH --nodes 1
+#SBATCH --gres=gpu:1
+#SBATCH --gpu-bind=none
+#SBATCH --mail-type=ALL
+
+MODEL_PARAMS=( diag_sheaf bundle_sheaf general_sheaf )
+DATASETS=( dblp acm imdb )
+
+NUM_TRIALS=10
+NUM_DATASETS=${#DATASETS[@]}
+
+IDX=${SLURM_ARRAY_TASK_ID}
+MODEL_IDX=$(( IDX % (NUM_TRIALS * NUM_DATASETS) ))
+DATA_IDX=$(( IDX % (NUM_TRIALS) ))
+
+MODEL=${MODEL_PARAMS[MODEL_IDX]}
+DATASET=${DATASETS[DATA_IDX]}
+
+export WANDB_API_KEY="cc080145b244f97b7db093ba0e3de5088e7ee7aa"
+srun python sheaf_nc.py model="${MODEL}" dataset="${DATASET}" +tag="${MODEL,DATASET,'exp1'}"
