ff1cf7403b7eb1f700fb8d5a38bb0eaf4f18afdd
Luke Braithwaite <lukebraithwaite7@gmail.com>
2024-01-25 14:30:48 +0000

    :sparkles: added code for GNN experiments

---
+++ slurm_scripts/run_gnn_nc_experiments.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#SBATCH -J sheafnc_baselines
+#SBATCH --output=sheafnc_baselines/%A_%a.out
+#SBATCH --error=sheafnc_baselines/%A_%a.err
+#SBATCH -A COMPUTERLAB-SL2-GPU
+#SBATCH --time=1:00:00
+#SBATCH -a 0-179%10
+#SBATCH -p ampere
+#SBATCH --nodes 1
+#SBATCH --gres=gpu:1
+#SBATCH --gpu-bind=none
+#SBATCH --mail-type=ALL
+
+MODEL_PARAMS=( HAN HGT HGCN RGCN GCN GAT )
+DATASETS=( DBLP ACM IMDB )
+
+N_TRIALS=10
+N_DATASETS=${#DATASETS[@]}
+N_MODELS=${#MODEL_PARAMS[@]}
+
+IDX=${SLURM_ARRAY_TASK_ID}
+N_RUN=$((IDX / N_TRIALS))
+MODEL_IDX=$(( N_RUN / N_DATASETS ))
+DATA_IDX=$(( N_RUN % N_DATASETS ))
+
+MODEL=${MODEL_PARAMS[MODEL_IDX]}
+DATASET=${DATASETS[DATA_IDX]}
+
+export WANDB_API_KEY="cc080145b244f97b7db093ba0e3de5088e7ee7aa"
+source ~/venv/bin/activate
+srun python node_classification.py model="${MODEL}" dataset="${DATASET}" tags=["${MODEL}","${DATASET}",nc,gnn,exp1]
