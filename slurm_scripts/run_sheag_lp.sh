13ef8d62c23728c74c255ebbefe0e7d7274f1fa6
Luke Braithwaite <lukebraithwaite7@gmail.com>
2024-02-06 21:03:36 +0000

    :hammer: slurm sheaf_lp profiling code test

---
+++ slurm_scripts/run_sheag_lp.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH -J sheaf_lp_profile
+#SBATCH --output=sheaf_lp_profile/out/%A_%a.out
+#SBATCH --error=sheaf_lp_profile/err/%A_%a.err
+#SBATCH -A COMPUTERLAB-SL2-GPU
+#SBATCH --time=3:00:00
+#SBATCH -a 0-89%10
+#SBATCH -p ampere
+#SBATCH --nodes 1
+#SBATCH --gres=gpu:1
+#SBATCH --gpu-bind=none
+#SBATCH --mail-type=ALL
+
+export WANDB_CACHE_DIR="~/rds/hpc-work/.wandb"
+export WANDB_API_KEY="cc080145b244f97b7db093ba0e3de5088e7ee7aa"
+source ~/venv/bin/activate
+sbatch run sheaf_lp.py
