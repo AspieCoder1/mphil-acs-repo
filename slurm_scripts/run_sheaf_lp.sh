496537a1e10eef85ee253e45bcc1f0e225166a80
Luke Braithwaite <lukebraithwaite7@gmail.com>
2024-02-06 21:21:06 +0000

    :ambulance: fixed slurm script

--- slurm_scripts/run_sheaf_lp.sh
+++ slurm_scripts/run_sheaf_lp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH -J sheaf_lp_profile
-#SBATCH --output=sheaf_lp_profile/out/%A_%a.out
-#SBATCH --error=sheaf_lp_profile/err/%A_%a.err
+#SBATCH --output=sheaf_lp_profile/out/%a.out
+#SBATCH --error=sheaf_lp_profile/err/%a.err
 #SBATCH -A COMPUTERLAB-SL2-GPU
 #SBATCH --time=3:00:00
 #SBATCH -p ampere
@@ -13,4 +13,4 @@
 export WANDB_CACHE_DIR="~/rds/hpc-work/.wandb"
 export WANDB_API_KEY="cc080145b244f97b7db093ba0e3de5088e7ee7aa"
 source ~/venv/bin/activate
-sbatch run sheaf_lp.py
+sbatch run python sheaf_lp.py
