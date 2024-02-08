5cb6df76ba5f37510ab8beb7d0a1d1110ba956e3
Luke Braithwaite <lukebraithwaite7@gmail.com>
2024-02-08 11:09:31 +0000

    :ambulance: printing checkpoint keys

--- tsne_nc.py
+++ tsne_nc.py
@@ -20,24 +20,25 @@ def main() -> None:
     checkpoint = torch.load(
         "sheafnc_checkpoints/kj4z929k/DiagSheaf-DBLP-epoch=191.ckpt")
     print(checkpoint.keys())
-    encoder = checkpoint["model"].to("cuda")
-
-    # 3) calculate the singular values
-    x_maps = F.dropout(data.x, 0, training=False)
-    maps = encoder.sheaf_learners[0](x_maps.reshape(encoder.graph_size, -1),
-                                     data.edge_index)
-    sdvals = torch.linalg.svdvals(maps).cpu().detach().numpy()
-    print(sdvals.shape)
-    tsne_outputs = TSNE(n_components=2).fit_transform(maps)
-
-    # 4) Plotting the stuff
-    sns.set_style('whitegrid')
-    sns.set_context('paper')
-    fig = plt.figure(figsize=(8, 8))
-    ax = fig.add_subplot(111)
-    ax.scatter(tsne_outputs[:, 0], tsne_outputs[:, 1], c=data.edge_types)
-    fig.savefig("tsne_diag_dblp.pdf", bbox_inches='tight')
-    fig.savefig("tsne_diag_dblp.png", bbox_inches='tight')
+    print(checkpoint['hyper_parameters'])
+    # encoder = checkpoint["model"].to("cuda")
+    #
+    # # 3) calculate the singular values
+    # x_maps = F.dropout(data.x, 0, training=False)
+    # maps = encoder.sheaf_learners[0](x_maps.reshape(encoder.graph_size, -1),
+    #                                  data.edge_index)
+    # sdvals = torch.linalg.svdvals(maps).cpu().detach().numpy()
+    # print(sdvals.shape)
+    # tsne_outputs = TSNE(n_components=2).fit_transform(maps)
+    #
+    # # 4) Plotting the stuff
+    # sns.set_style('whitegrid')
+    # sns.set_context('paper')
+    # fig = plt.figure(figsize=(8, 8))
+    # ax = fig.add_subplot(111)
+    # ax.scatter(tsne_outputs[:, 0], tsne_outputs[:, 1], c=data.edge_types)
+    # fig.savefig("tsne_diag_dblp.pdf", bbox_inches='tight')
+    # fig.savefig("tsne_diag_dblp.png", bbox_inches='tight')
 
 
 if __name__ == '__main__':
