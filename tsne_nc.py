9302121e5711afae6b1ab1798c0c38ace1ba0eee
Luke Braithwaite <lukebraithwaite7@gmail.com>
2024-02-08 10:01:00 +0000

    :sparkles: TSNE for sheaf_nc diag_sheaf DBLP test

---
+++ tsne_nc.py
@@ -0,0 +1,31 @@
+import matplotlib.pyplot as plt
+import torch
+import torch.nn.functional as F
+from sklearn.manifold import TSNE
+from torch_geometric.data import Data
+
+from datasets.hgb import DBLPDataModule
+from models.SheafNodeClassifier import SheafNodeClassifier
+
+
+def main():
+    dm = DBLPDataModule(homogeneous=True)
+    dm.prepare_data()
+    data: Data = dm.pyg_datamodule.data
+    model = SheafNodeClassifier.load_from_checkpoint(
+        "sheafnc_checkpoints/kj4z929k/DiagSheaf-DBLP-epoch=191.ckpt")
+    x_maps = F.dropout(data.x, 0, training=False)
+    maps = model.encoder.sheaf_learners[0](x_maps.reshape(model.graph_size, -1),
+                                           data.edge_index)
+    sdvals = torch.linalg.svdvals(maps).cpu().detach().numpy()
+    print(sdvals.shape)
+    tsne_outputs = TSNE(n_components=2).fit_transform(maps)
+
+    fig = plt.figure(figsize=(8, 8))
+    ax = fig.add_subplot(111)
+    ax.scatter(tsne_outputs[:, 0], tsne_outputs[:, 1], c=data.edge_types)
+    fig.savefig("tsne_diag_dblp.pdf")
+
+
+if __name__ == '__main__':
+    main()
