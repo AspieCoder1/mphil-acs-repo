c417339cfe414d8661269d8ee58f1715666080dc
Luke Braithwaite <lukebraithwaite7@gmail.com>
2024-02-08 14:30:41 +0000

    :sparkles: increased time limit on slurm script as t-SNE is expensive

--- slurm_scripts/run_tsne_nc.sh
+++ slurm_scripts/run_tsne_nc.sh
@@ -3,7 +3,7 @@
 #SBATCH --output=sheafnc_tsne/out/%A.out
 #SBATCH --error=sheafnc_tsne/err/%A.err
 #SBATCH -A COMPUTERLAB-SL2-GPU
-#SBATCH --time=1:00:00
+#SBATCH --time=3:00:00
 #SBATCH -p ampere
 #SBATCH --nodes 1
 #SBATCH --gres=gpu:1
