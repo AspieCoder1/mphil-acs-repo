971d78e6de880d2b9adf92cf09d87242a741d989
Luke Braithwaite <lukebraithwaite7@gmail.com>
2024-02-09 21:58:26 +0000

    :ambulance: fixed slurm submission script for cclake-himem partition

--- slurm_scripts/process_tsne.sh
+++ slurm_scripts/process_tsne.sh
@@ -4,10 +4,10 @@
 #SBATCH --error=process_tsne/err/%A.err
 #SBATCH -A COMPUTERLAB-SL2-CPU
 #SBATCH --time=10:00:00
-#SBATCH -p icelake-himem
+#SBATCH -p cclake-himem
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --mem=32000
+#SBATCH --mem=16000
 
 source ~/venv/bin/activate
 srun python process_tsne.py
