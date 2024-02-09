81fef2a58cb3d87b1fadf5b2a70abd624387a729
Luke Braithwaite <lukebraithwaite7@gmail.com>
2024-02-09 21:44:54 +0000

    :ambulance: added more memory for cpu

--- slurm_scripts/process_tsne.sh
+++ slurm_scripts/process_tsne.sh
@@ -4,9 +4,10 @@
 #SBATCH --error=process_tsne/err/%A.err
 #SBATCH -A COMPUTERLAB-SL2-CPU
 #SBATCH --time=10:00:00
-#SBATCH -p cclake-himem
+#SBATCH -p icelake-himem
 #SBATCH --nodes 1
-#SBATCH --ntasks=10
+#SBATCH --mem=32000
+#SBATCH
 
 module load gcc/11
 source ~/venv/bin/activate
