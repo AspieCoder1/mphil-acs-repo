8ae69d4b220267181c5e8924a096d799c143d647
Luke Braithwaite <lukebraithwaite7@gmail.com>
2024-02-10 14:34:21 +0000

    :zap: using only a subsample of edges

--- process_tsne.py
+++ process_tsne.py
@@ -2,7 +2,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
 from sklearn.decomposition import PCA
-from sklearn.model_selection import train_test_split
 
 
 def main():
@@ -13,23 +12,23 @@ def main():
 
     print("Loaded arrays")
 
-    singular_values, _, edge_types, _ = train_test_split(singular_values, edge_types,
-                                                         train_size=0.1,
-                                                         stratify=edge_types,
-                                                         random_state=42)
+    shuffled_idx = np.random.permutation(np.arange(len(edge_types)))
 
-    # tsne_outputs = PCA(n_components=2).fit_transform(
-    #     singular_values)
-    #
-    # # 5) Plotting the stuff
-    # sns.set_style('whitegrid')
-    # sns.set_context('paper')
-    # fig = plt.figure(figsize=(8, 8))
-    # ax = fig.add_subplot(111)
-    # ax.scatter(tsne_outputs[:, 0], tsne_outputs[:, 1], c=edge_types)
-    # ax.legend(name="Edge type")
-    # fig.savefig("tsne-plots/pca_diag_dblp.pdf", bbox_inches='tight')
-    # fig.savefig("tsne-plots/pca_diag_dblp.png", bbox_inches='tight')
+    singular_values = singular_values[shuffled_idx][:1_000]
+    edge_types = edge_types[shuffled_idx][:1_000]
+
+    tsne_outputs = PCA(n_components=2).fit_transform(
+        singular_values)
+
+    # 5) Plotting the stuff
+    sns.set_style('whitegrid')
+    sns.set_context('paper')
+    fig = plt.figure(figsize=(8, 8))
+    ax = fig.add_subplot(111)
+    ax.scatter(tsne_outputs[:, 0], tsne_outputs[:, 1], c=edge_types)
+    ax.legend(name="Edge type")
+    fig.savefig("tsne-plots/pca_diag_dblp.pdf", bbox_inches='tight')
+    fig.savefig("tsne-plots/pca_diag_dblp.png", bbox_inches='tight')
 
 
 if __name__ == '__main__':
