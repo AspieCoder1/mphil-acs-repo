0c187be8fb7e3eaf9427ac01e285f9e85e40905b
Luke Braithwaite <lukebraithwaite7@gmail.com>
2024-02-10 14:28:22 +0000

    :bug: removed the PCA part to see what causes the seg_fault

--- process_tsne.py
+++ process_tsne.py
@@ -18,18 +18,18 @@ def main():
                                                          stratify=edge_types,
                                                          random_state=42)
 
-    tsne_outputs = PCA(n_components=2).fit_transform(
-        singular_values)
-
-    # 5) Plotting the stuff
-    sns.set_style('whitegrid')
-    sns.set_context('paper')
-    fig = plt.figure(figsize=(8, 8))
-    ax = fig.add_subplot(111)
-    ax.scatter(tsne_outputs[:, 0], tsne_outputs[:, 1], c=edge_types)
-    ax.legend(name="Edge type")
-    fig.savefig("tsne-plots/pca_diag_dblp.pdf", bbox_inches='tight')
-    fig.savefig("tsne-plots/pca_diag_dblp.png", bbox_inches='tight')
+    # tsne_outputs = PCA(n_components=2).fit_transform(
+    #     singular_values)
+    #
+    # # 5) Plotting the stuff
+    # sns.set_style('whitegrid')
+    # sns.set_context('paper')
+    # fig = plt.figure(figsize=(8, 8))
+    # ax = fig.add_subplot(111)
+    # ax.scatter(tsne_outputs[:, 0], tsne_outputs[:, 1], c=edge_types)
+    # ax.legend(name="Edge type")
+    # fig.savefig("tsne-plots/pca_diag_dblp.pdf", bbox_inches='tight')
+    # fig.savefig("tsne-plots/pca_diag_dblp.png", bbox_inches='tight')
 
 
 if __name__ == '__main__':
