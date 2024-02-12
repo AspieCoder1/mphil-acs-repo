25462ccffdad46f49ba9404752eb65538ff2047a
Luke Braithwaite <lukebraithwaite7@gmail.com>
2024-02-12 11:06:00 +0000

    :zap: reduced number of vectors plotted

--- process_tsne.py
+++ process_tsne.py
@@ -16,8 +16,8 @@ def main():
 
     shuffled_idx = rng.permutation(np.arange(len(edge_types)))
 
-    singular_values = singular_values[shuffled_idx][:10_000]
-    edge_types = edge_types[shuffled_idx][:10_000]
+    singular_values = singular_values[shuffled_idx][:5_000]
+    edge_types = edge_types[shuffled_idx][:5_000]
 
     umap_reducer = UMAP(random_state=42)
     embedding = umap_reducer.fit_transform(singular_values, edge_types)
@@ -29,7 +29,6 @@ def main():
     ax = fig.add_subplot(111)
 
     ax.scatter(embedding[:, 0], embedding[:, 1], c=edge_types, cmap='Spectral', s=5)
-    ax.gca().set_aspect('equal', 'datalim')
     fig.savefig("tsne-plots/umap_diag_dblp.pdf", bbox_inches='tight', dpi=300)
     fig.savefig("tsne-plots/umap_diag_dblp.png", bbox_inches='tight', dpi=300)
     print("Plotting finished")
