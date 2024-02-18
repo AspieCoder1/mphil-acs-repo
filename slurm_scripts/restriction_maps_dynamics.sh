3b84ef85e06f794f6e8ade9aad0e517cec769e6e
Luke Braithwaite <lukebraithwaite7@gmail.com>
2024-02-18 19:58:30 +0000

    :sparkles: added script for dynamics of restriction maps during training

---
+++ slurm_scripts/restriction_maps_dynamics.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#SBATCH -J preprocess_umap
+#SBATCH --output=preprocess_umap/out/%A_%a.out
+#SBATCH --error=preprocess_umap/err/%A_%a.err
+#SBATCH -A COMPUTERLAB-SL2-GPU
+#SBATCH --time=3:00:00
+#SBATCH -a 0-8
+#SBATCH -p ampere
+#SBATCH --nodes 1
+#SBATCH --gres=gpu:1
+#SBATCH --gpu-bind=none
+#SBATCH --mail-type=ALL
+
+MODEL_PARAMS=( diag_sheaf bundle_sheaf general_sheaf )
+DATASETS=( dblp acm imdb )
+
+IDX=${SLURM_ARRAY_TASK_ID}
+
+N_MODELS=${#MODELS[@]}
+MODEL_IDX=$(( IDX / N_MODELS ))
+DATA_IDX=$(( IDX % N_MODELS ))
+DATASET=${DATASETS[DATA_IDX]}
+MODEL=${MODELS[MODEL_IDX]}
+
+export WANDB_CACHE_DIR="~/rds/hpc-work/.wandb"
+export WANDB_API_KEY="cc080145b244f97b7db093ba0e3de5088e7ee7aa"
+source ~/venv/bin/activate
+srun python sheaf_nc.py model="${MODEL}" dataset="${DATASET}" tags=["${MODEL}","${DATASET}",nc,sheaf,exp3]
