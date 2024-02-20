de1a8acecfec0cc141efd637a2be7af25d621daf
Luke Braithwaite <lukebraithwaite7@gmail.com>
2024-02-20 23:32:58 +0000

    :sparkles: added pipeline for restriction map plotting

--- pl_callbacks/restriction_map_callback.py
+++ pl_callbacks/restriction_map_callback.py
@@ -5,6 +5,8 @@ import lightning as L
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
 from torch_geometric.data import Data
 
 from models.sheaf_node_classifier import TrainStepOutput
@@ -12,7 +14,10 @@ from models.sheaf_node_classifier import TrainStepOutput
 
 class RestrictionMapCallback(L.Callback):
     def __init__(self):
-        self.classifier = LogisticRegression()
+        self.pipeline = make_pipeline(
+            StandardScaler(),
+            LogisticRegression(max_iter=1_000),
+        )
 
     def on_train_batch_end(
         self,
@@ -24,12 +29,12 @@ class RestrictionMapCallback(L.Callback):
     ) -> None:
         X_train, X_test, y_train, y_test = train_test_split(
             outputs['restriction_maps'].cpu().detach().numpy(),
-            batch.edge_type.cpu().detach().numpy()
+            batch.edge_type.cpu().detach().numpy(),
         )
 
-        self.classifier.fit(X_train, y_train)
+        self.pipeline.fit(X_train, y_train)
 
-        preds = self.classifier.predict(X_test)
+        preds = self.pipeline.predict(X_test)
 
         acc = accuracy_score(y_test, preds)
 
