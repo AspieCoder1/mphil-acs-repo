16dca5d65713de6a468db618540635d419757b12
Luke Braithwaite <lukebraithwaite7@gmail.com>
2024-02-29 09:45:40 +0000

    :sparkles: slurm script for running SheafGNN on LP tasks

--- slurm_scripts/sheaf_lp_baselines.sh
+++ slurm_scripts/sheaf_lp_baselines.sh
@@ -4,18 +4,35 @@
 # License: MIT
 #
 
-#SBATCH -J sheaf_lp_profile
-#SBATCH --output=slurm_outputs/sheaf_lp_profile/out/%a.out
-#SBATCH --error=slurm_outputs/sheaf_lp_profile/err/%a.err
+#SBATCH -J sheaf_link_baselines
+#SBATCH --output=slurm_outputs/sheaf_link_baselines/out/%a.out
+#SBATCH --error=slurm_outputs/sheaf_link_baselines/err/%a.err
 #SBATCH -A COMPUTERLAB-SL2-GPU
-#SBATCH --time=00:10:00
+#SBATCH --time=00
+#SBATCH -a 0-59
 #SBATCH -p ampere
 #SBATCH --nodes 1
 #SBATCH --gres=gpu:1
 #SBATCH --gpu-bind=none
 #SBATCH --mail-type=ALL
 
+MODEL_PARAMS=( bundle_sheaf diag_sheaf general_sheaf )
+DATASETS=( last_fm amazon_books )
+
+N_TRIALS=10
+N_DATASETS=${#DATASETS[@]}
+N_MODELS=${#MODEL_PARAMS[@]}
+
+IDX=${SLURM_ARRAY_TASK_ID}
+N_RUN=$((IDX / N_TRIALS))
+MODEL_IDX=$(( N_RUN / N_DATASETS ))
+DATA_IDX=$(( N_RUN % N_DATASETS ))
+
+MODEL=${MODEL_PARAMS[MODEL_IDX]}
+DATASET=${DATASETS[DATA_IDX]}
+
+
 export WANDB_CACHE_DIR="~/rds/hpc-work/.wandb"
 export WANDB_API_KEY="cc080145b244f97b7db093ba0e3de5088e7ee7aa"
 source ~/venv/bin/activate
-srun  python sheaf_lp.py dataset='amazon_books'
+srun  python sheaf_lp.py dataset="${DATASET}" model="${MODEL}" +tags=["${MODEL}","${DATASET}",lp,sheaf,exp2]
