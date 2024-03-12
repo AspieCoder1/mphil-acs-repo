93bb92cc80b22df8683fae340a7005d66c6b05ab
Luke Braithwaite <lukebraithwaite7@gmail.com>
2024-03-12 15:15:49 +0000

    :ambulance: fixed error with restriction map legend error being raised by pytorch

--- pl_callbacks/restriction_map_callback.py
+++ pl_callbacks/restriction_map_callback.py
@@ -133,12 +133,13 @@ class RestrictionMapUMAP(L.Callback):
         edge_types_to_label = {}
 
         edge = batch.edge_index[unique_index]
-        src_type = batch.node_type[edge[:, 0]]
-        dst_type = batch.node_type[edge[:, 1]]
+        src_type = batch.node_type[edge[:, 0]].detach().cpu().numpy()
+        dst_type = batch.node_type[edge[:, 1]].detach().cpu().numpy()
 
         for i, edge_type in enumerate(unique_edge_types):
-            src = src_type[i].item()
-            dst = dst_type[i].item()
+            src = src_type[unique_index[i]]
+            dst = dst_type[unique_index[i]]
+            print(rf"{src: d} \to {dst: d}")
             edge_types_to_label[edge_type] = rf"{src: d} \to {dst: d}"
 
         scatter = ax.scatter(
