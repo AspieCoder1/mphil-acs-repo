4fbd700dda00dffcb9e1bcc8114c598491c5a1de
Luke Braithwaite <lukebraithwaite7@gmail.com>
2024-03-22 22:24:41 +0000

    :sparkles: sheaf_nc_baselines on SL2 code

--- slurm_scripts/sheaf_nc_baselines.sh
+++ slurm_scripts/sheaf_nc_baselines.sh
@@ -7,7 +7,7 @@
 #SBATCH -J sheafnc_baselines
 #SBATCH --output=slurm_outputs/sheafnc_baselines/out/%A_%a.out
 #SBATCH --error=slurm_outputs/sheafnc_baselines/err/%A_%a.err
-#SBATCH -A COMPUTERLAB-SL3-GPU
+#SBATCH -A COMPUTERLAB-SL2-GPU
 #SBATCH --time=3:00:00
 #SBATCH -a 0-89%10
 #SBATCH -p ampere
@@ -34,4 +34,4 @@ DATASET=${DATASETS[DATA_IDX]}
 export WANDB_CACHE_DIR="~/rds/hpc-work/.wandb"
 export WANDB_API_KEY="cc080145b244f97b7db093ba0e3de5088e7ee7aa"
 source ~/venv/bin/activate
-srun python sheaf_nc.py model="${MODEL}" dataset="${DATASET}" sheaf_learner=type_concat tags=["${MODEL}","${DATASET}",nc,sheaf,exp1_1,type_concat]
+srun python sheaf_nc.py model="${MODEL}" dataset="${DATASET}" sheaf_learner=local_concat tags=["${MODEL}","${DATASET}",nc,sheaf,exp1_1,type_concat]
