3d3b421440b2664dacda776050ddaee57b9b57c2
Luke Braithwaite <lukebraithwaite7@gmail.com>
2024-03-25 15:23:50 +0000

    :recycle: baselines -> gnn_baselines

---
+++ models/gnn_baselines/__init__.py
@@ -0,0 +1,9 @@
+#  Copyright (c) 2024. Luke Braithwaite
+#  License: MIT
+
+from .GAT import GAT
+from .GCN import GCN
+from .HAN import HAN
+from .HeteroGNN import HeteroGNN
+from .HGT import HGT
+from .RGCN import RGCN
