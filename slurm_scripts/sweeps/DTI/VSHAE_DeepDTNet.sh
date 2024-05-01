960d4edcc8b0ea520b197edfbad1d4d548557cd4
Luke Braithwaite <lukebraithwaite7@gmail.com>
2024-05-01 14:28:06 +0100

    :ambulance: fixed slurm script

--- slurm_scripts/sweeps/DTI/VSHAE_DeepDTNet.sh
+++ slurm_scripts/sweeps/DTI/VSHAE_DeepDTNet.sh
@@ -4,7 +4,7 @@
 #SBATCH --output=slurm_output/sweeps/VSHAE_DeepDTNet/out/%A_%a.out
 #SBATCH --error=slurm_output/sweeps/VSHAE_DeepDTNet/err/%A_%a.err
 #SBATCH -A COMPUTERLAB-SL2-GPU
-#SBATCH --time=1:00:00
+#SBATCH --time=00:15:00
 #SBATCH -a 0-60
 #SBATCH -p ampere
 #SBATCH --nodes 1
@@ -14,4 +14,4 @@
 
 export WANDB_API_KEY="cc080145b244f97b7db093ba0e3de5088e7ee7aa"
 source ~/venv/bin/activate
-srun python -m wandb agent "acs-thesis-lb2027/gnn-baselines/oqxomqkz" --count 1
+srun python -m wandb agent --count 1 "acs-thesis-lb2027/gnn-baselines/oqxomqkz"
