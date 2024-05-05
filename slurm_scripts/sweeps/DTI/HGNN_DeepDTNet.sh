3ae9b5f170fec7b0faf00306cf4257be5a6530ce
Luke Braithwaite <lukebraithwaite7@gmail.com>
2024-05-05 14:45:38 +0100

    :ambulance: fixed HGNN issues

--- slurm_scripts/sweeps/DTI/HGNN_DeepDTNet.sh
+++ slurm_scripts/sweeps/DTI/HGNN_DeepDTNet.sh
@@ -19,4 +19,4 @@
 
 export WANDB_API_KEY="cc080145b244f97b7db093ba0e3de5088e7ee7aa"
 source ~/venv/bin/activate
-srun python -m wandb agent --count 1 "acs-thesis-lb2027/gnn-baselines/hzbdv67l"
+srun python -m wandb agent --count 1 "acs-thesis-lb2027/gnn-baselines/mzw1tc66"
