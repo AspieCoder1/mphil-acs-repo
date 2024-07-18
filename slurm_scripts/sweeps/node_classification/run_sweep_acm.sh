7ffbff4bd6da13d87137a5dde0513546b603705d
Luke Braithwaite <lukebraithwaite7@gmail.com>
2024-07-18 18:22:07 +0100

    :wrench: added sweep configs for IMDB, ACM + DBLP

---
+++ slurm_scripts/sweeps/node_classification/run_sweep_acm.sh
@@ -0,0 +1,20 @@
+#
+# Copyright (c) 2024. Luke Braithwaite
+# License: MIT
+#
+
+#SBATCH -J ACM_Sweep
+#SBATCH --output=slurm_output/sweeps/ACM/out/%A_%a.out
+#SBATCH --error=slurm_output/sweeps/ACM/err/%A_%a.err
+#SBATCH -A plio-sl2-gpu
+#SBATCH --time=00:15:00
+#SBATCH -a 0-60
+#SBATCH -p ampere
+#SBATCH --nodes 1
+#SBATCH --gres=gpu:1
+#SBATCH --gpu-bind=none
+#SBATCH --mail-type=ALL
+
+export WANDB_API_KEY="cc080145b244f97b7db093ba0e3de5088e7ee7aa"
+source ~/venv/bin/activate
+srun python -m wandb agent --count 1 "acs-thesis-lb2027/hetero-sheaf-paper/7q5o0xl5"
