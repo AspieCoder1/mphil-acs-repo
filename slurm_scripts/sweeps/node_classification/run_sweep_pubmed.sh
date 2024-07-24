5acf462648edf3263a0aed1e2e155f8d51fd282e
Luke Braithwaite <lukebraithwaite7@gmail.com>
2024-07-24 09:44:14 +0100

    :wrench: set up sweep configs for NC

--- slurm_scripts/sweeps/node_classification/run_sweep_pubmed.sh
+++ slurm_scripts/sweeps/node_classification/run_sweep_pubmed.sh
@@ -9,7 +9,7 @@
 #SBATCH --error=slurm_output/sweeps/PubMedNC/err/%A_%a.err
 #SBATCH -A plio-sl2-gpu
 #SBATCH --time=00:15:00
-#SBATCH -a 0-99
+#SBATCH -a 0-199
 #SBATCH -p ampere
 #SBATCH --nodes 1
 #SBATCH --gres=gpu:1
@@ -18,4 +18,4 @@
 
 export WANDB_API_KEY="cc080145b244f97b7db093ba0e3de5088e7ee7aa"
 source ~/venv/bin/activate
-srun python -m wandb agent --count 1 "acs-thesis-lb2027/hetero-sheaf-paper/361h3jse"
+srun python -m wandb agent --count 1 "acs-thesis-lb2027/hetero-sheaf-paper/4sl4f8sx"
