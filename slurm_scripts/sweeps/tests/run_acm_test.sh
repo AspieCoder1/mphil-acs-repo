c3c3dea51d7d352da218cbd0511e3c7f602610f3
Luke Braithwaite <lukebraithwaite7@gmail.com>
2024-08-24 11:34:53 +0100

    :sparkles: added test sweep slurm script

---
+++ slurm_scripts/sweeps/tests/run_acm_test.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+# Copyright (c) 2024. Luke Braithwaite
+# License: MIT
+#
+
+#SBATCH -J ACM_DiagSheaf_Test_Sweep
+#SBATCH --output=slurm_output/sweeps/ACM_DiagSheaf_Test/out/%A_%a.out
+#SBATCH --error=slurm_output/sweeps/ACM_DiagSheaf_Test/err/%A_%a.err
+#SBATCH -A plio-sl2-gpu
+#SBATCH --time=00:15:00
+#SBATCH -a 0-49
+#SBATCH -p ampere
+#SBATCH --nodes 1
+#SBATCH --gres=gpu:1
+#SBATCH --gpu-bind=none
+#SBATCH --mail-type=ALL
+
+export WANDB_CACHE_DIR=".wandb"
+export WANDB_DIR=".wandb"
+export WANDB_DATA_DIR=".wandb"
+export WANDB_API_KEY="cc080145b244f97b7db093ba0e3de5088e7ee7aa"
+source ~/venv/bin/activate
+srun python -m wandb agent --count 1 "acs-thesis-lb2027/hetero-sheaf-paper/0ngtm5qb"
